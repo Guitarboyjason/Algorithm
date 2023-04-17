94bc18ba7136f63dfe3a6693ce9be89c3a445ab2
이태경 <61171399+Guitarboyjason@users.noreply.github.com>
2023-04-18 08:56:57 +0900

    [Silver I] Title: 회의실 배정, Time: 432 ms, Memory: 133780 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,31 +1,12 @@
-# def find_max_time(arr,endtime):
-
-
+import sys
+input = sys.stdin.readline
 
 N = int(input())
 
-arr = []
-for i in range(N):
-    start , finish = map(int,input().split())
-    arr.append([start, finish])
-
+arr = [list(map(int,input().split()))for _ in range(N)]
 arr.sort(key=lambda x:x[0])
 arr.sort(key=lambda x:x[1])
-# tmp_arr = arr[:]
-# find = -1
-# for i in tmp_arr:
-#     if i[0] == find:
-#         arr.remove(i)
-#     else:
-#         find = i[0]
 
-# tmp_arr = arr[:]
-# find = arr[-1][1]
-# for i in range(1,len(arr)):#range(1,3)
-#     if arr[len(arr)-i-1][1] > find: #[1][1], [0][1]
-#         tmp_arr.remove(arr[len(arr)-i-1])
-#     else:
-#         find = tmp_arr[len(arr)-i-1][1]
 last = 0
 count = 0
 for i,j in arr:
