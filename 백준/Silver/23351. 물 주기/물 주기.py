5cb5ccc9674a493cc5d51690cadd46ac57c15d92
이태경 <61171399+Guitarboyjason@users.noreply.github.com>
2023-04-25 08:17:08 +0900

    [Silver III] Title: 물 주기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/23351. 물 주기/물 주기.py
@@ -0,0 +1,20 @@
+# 모든 캣닢이 살아있는 기간이 최대한 길어지도록 물을 준다. 첫 캣닢이 죽는 날짜?
+# 그럼 수분이 가장 적은 화분에 물을 줘야겠네, 근데 연속된 A개의 화분에 물을 준다....
+# 연속된이 안겹치도록 물을 줄 때를 확인해보자
+N,K,A,B = map(int,input().split())
+cycle = N//A+1 if  N / A != N // A else N//A
+# print(cycle)
+# print(K/(B*cycle))
+cnt = 0
+cnt_cycle = 0
+while K >0:
+    # K -= cycle
+    # K += B
+    # cnt += 1
+    K -= 1
+    cnt_cycle += 1
+    if cnt_cycle == cycle:
+        K += B
+        cnt_cycle = 0
+    cnt += 1
+print(cnt)
