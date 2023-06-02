664d360f3bd913f7b84fa829fd826c17a0d4c6f9
이태경 <61171399+Guitarboyjason@users.noreply.github.com>
2023-06-02 09:03:59 +0900

    [Silver II] Title: 주식, Time: 4576 ms, Memory: 174956 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,31 +1,18 @@
-"""
-이후 최고점보다 낮은 가격들에 대해 사모으고
-최고점에서 판다.
-"""
 import sys
-
-input = sys.stdin.readline
-
+inpurt = sys.stdin.readline
 for _ in range(int(input())):
     N = int(input())
-    price = list(map(int, input().split()))
-    max_list = []
+    price = list(map(int,input().split()))
+    answer = 0
+    
+    dp = [-1 for _ in range(N)]
     maximum = -1
-    for i in range(N - 1, -1, -1):
-        if price[i] > maximum:
+    for i in range(N-1,-1,-1):
+        if maximum < price[i]:
             maximum = price[i]
-        max_list.append(maximum)
-    max_list.reverse()
-    stocks = 0
-    money = 0
-    for i in range(N - 1):
-        # if price[i] > price[i + 1]:
-        if price[i] < max_list[i]:
-            money -= price[i]
-            stocks += 1
-            # tmp_money = 0
-        else:
-            money += stocks * price[i]
-            stocks = 0
-    money += stocks * price[-1]
-    print(money)
+        dp[i] = maximum
+    
+    for i in range(N-1):
+        if price[i] < dp[i]:
+            answer += dp[i] - price[i]
+    print(answer)
