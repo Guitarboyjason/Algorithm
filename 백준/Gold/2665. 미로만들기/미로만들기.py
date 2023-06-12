22fdcfab8b77dd48d0bc9115bb88a2eaef2e3004
이태경 <61171399+Guitarboyjason@users.noreply.github.com>
2023-06-12 09:05:43 +0900

    [Gold IV] Title: 미로만들기, Time: 112 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -3,31 +3,35 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-graph = [list(input()) for _ in range(n)]
 
-# 벽을 뚫고 들어가는 횟수를 확인한다...
-directions = [
-    (i, j) for j in range(-1, 2) for i in range(-1, 2) if i != j and (i == 0 or j == 0)
-]
-# print(directions)
-visited = [[3000 for _ in range(n)] for _ in range(n)]
-visited[0][0] = 0
-# root = [0,0]
+def pre_processing():
+    n = int(input())
+    graph = [list(input()) for _ in range(n)]
+    return n, graph
 
-queue = deque()
-queue.append((0, 0, 0))  # x,y,cnt
 
-while queue:
-    x, y, cnt = queue.popleft()
-    for nx, ny in directions:
-        nx += x
-        ny += y
-        if 0 <= nx < n and 0 <= ny < n:
-            if graph[nx][ny] == "1" and visited[nx][ny] > cnt:
-                visited[nx][ny] = cnt
-                queue.append((nx, ny, cnt))
-            elif graph[nx][ny] == "0" and visited[nx][ny] > cnt + 1:
-                visited[nx][ny] = cnt + 1
-                queue.append((nx, ny, cnt + 1))
-print(visited[n - 1][n - 1])
+def dijkstra():
+    queue = deque()
+    queue.append((0, 0, 0))  # x,y,cnt
+
+    while queue:
+        x, y, cnt = queue.popleft()
+        for nx, ny in directions:
+            nx += x
+            ny += y
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] == "1" and visited[nx][ny] > cnt:
+                    visited[nx][ny] = cnt
+                    queue.append((nx, ny, cnt))
+                elif graph[nx][ny] == "0" and visited[nx][ny] > cnt + 1:
+                    visited[nx][ny] = cnt + 1
+                    queue.append((nx, ny, cnt + 1))
+    print(visited[n - 1][n - 1])
+
+
+if __name__ == "__main__":
+    n, graph = pre_processing()
+    directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+    visited = [[3000 for _ in range(n)] for _ in range(n)]
+    visited[0][0] = 0
+    dijkstra()
