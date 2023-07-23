6af5c17a8d74764c5cc4840d962f95d26c11e743
이태경 <61171399+Guitarboyjason@users.noreply.github.com>
2023-07-23 21:47:42 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 248 ms, Memory: 18504 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String hong = sc.next();
+		String fan = ":fan:";
+		System.out.println(fan + fan + fan);
+		System.out.println(fan + ":" + hong + ":" + fan);
+		System.out.println(fan + fan + fan);
+	}
+}
