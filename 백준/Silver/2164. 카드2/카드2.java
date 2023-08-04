a93f68a8ba1fdffd64d0b7bb6910537b17ce047f
이태경 <61171399+Guitarboyjason@users.noreply.github.com>
2023-08-04 16:03:05 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 28864 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+
+		Queue<Integer> queue = new ArrayDeque<>();
+
+		for (int i = 1; i <= N; i++) {
+			queue.offer(i);
+		}
+		while (queue.size() > 1) {
+			queue.poll();
+			queue.offer(queue.poll());
+
+		}
+
+		System.out.println(queue.poll());
+	}
+}
