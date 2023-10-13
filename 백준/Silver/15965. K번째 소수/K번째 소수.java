b7e44c8e4b6ca76e68ff44932816d9adecd8c924
이태경 <61171399+Guitarboyjason@users.noreply.github.com>
2023-10-13 22:47:29 +0900

    [Silver II] Title: K번째 소수, Time: 232 ms, Memory: 42072 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.java
+++ 백준/Silver/15965. K번째 소수/K번째 소수.java
@@ -1,39 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    static boolean [] prime_num;
-    public static void main(String[] args) throws IOException {
-        prime_num = new boolean[500_001];
-        for(int i = 0 ; i < 500_001; i ++){
-            prime_num[i] = true;
-        }
-        prime_num[0] = false;
-        prime_num[1] = false;
+	static boolean prime[] = new boolean[8000002];
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int K = Integer.parseInt(br.readLine());
-        ArrayList<Integer> prime = new ArrayList<>();
-        for(int i = 2; i <= (int)Math.pow(500_000,0.5) ; i ++){
-//            if (!prime_num[i])
-//                continue;
-//            prime.add(i);
-            if (prime_num[i]) {
-                for (int j = i+i; j < 500_000; j += i) {
-                    prime_num[j] = false;
-                }
-            }
-        }
-//        System.out.println(prime);
-        for(int i = 0 ; i <= 500_000 ; i ++){
-            if(prime_num[i])
-//                System.out.println(prime_num[i]);
-                prime.add(i);
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println(prime.get(K-1));
-    }
+		int n = Integer.parseInt(br.readLine());
+		List<Long> list = new ArrayList<Long>();
+
+		prime();
+
+		for (long i = 2; i < 8000000; i++) {
+			if (!prime[(int) i]) {
+				list.add(i);
+			}
+		}
+
+		System.out.println(list.get(n - 1));
+	}
+
+	static void prime() {
+		prime[0] = prime[1] = true;
+
+		for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if (prime[i]) {
+				continue;
+			}
+
+			for (int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
+	}
 }
