65bc46e3cc4c91c0d8296af9ebb338e9a515dbb0
이태경 <61171399+Guitarboyjason@users.noreply.github.com>
2023-10-13 22:38:29 +0900

    [Silver II] Title: K번째 소수, Time: 164 ms, Memory: 18608 KB, Score: 25 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.java
+++ 백준/Silver/15965. K번째 소수/K번째 소수.java
@@ -14,17 +14,25 @@ public class Main {
         prime_num[0] = false;
         prime_num[1] = false;
 
-        ArrayList<Integer> prime = new ArrayList<>();
-        for(int i = 2; i <= 500_000/2 ; i ++){
-            if (!prime_num[i])
-                continue;
-            prime.add(i);
-            for(long j = i*2; j < 500_001; j += i){
-                prime_num[(int)j] = false;
-            }
-        }
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int K = Integer.parseInt(br.readLine());
+        ArrayList<Integer> prime = new ArrayList<>();
+        for(int i = 2; i <= (int)Math.pow(500_000,0.5) ; i ++){
+//            if (!prime_num[i])
+//                continue;
+//            prime.add(i);
+            if (prime_num[i]) {
+                for (int j = i+i; j < 500_000; j += i) {
+                    prime_num[j] = false;
+                }
+            }
+        }
+//        System.out.println(prime);
+        for(int i = 0 ; i <= 500_000 ; i ++){
+            if(prime_num[i])
+//                System.out.println(prime_num[i]);
+                prime.add(i);
+        }
 
         System.out.println(prime.get(K-1));
     }
